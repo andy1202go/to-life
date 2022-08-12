5817606fb8ef03adb9d0ac8209d12076ce993e83
80338398 <liangbo@oppo.com>
2022-08-12 11:13:22 +0800

    mine first after given

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "to-life",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/to-life/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -25,10 +25,10 @@ module.exports = {
         needComments: true,
         // 其他的 Vssue 配置
         autoCreateIssue: true,
-        clientId: "adb9fb0ac1159e00ce7f",
-        clientSecret: "27da8dc85f808c2bd1b6e44da5ae69c4ddf17d8d",
+        clientId: "a1e50b15b33c9a145f07",
+        clientSecret: "a8d767c35ee5fe666dfcb05c127d4f814a342080",
         owner: "eryajf",
-        repo: "vdoing-template",
+        repo: "to-life",
       },
     ],
     // 增强 markdown
