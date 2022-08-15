e96e1abc3107f976f7ec2a375dabe9646358af5c
80338398 <liangbo@oppo.com>
2022-08-15 15:10:09 +0800

    nav and review

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,6 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: '啥都有',link: '/one/',},
   { text: '留言板', link: '/message-board/' },
-  { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
