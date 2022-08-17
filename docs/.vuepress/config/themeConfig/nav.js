80dcd8b81a8563244fc4e9d4dd54c065124c32b6
andy1202go <andy1202go@163.com>
2022-08-17 22:33:14 +0800

    目录结构

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,6 +1,11 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '啥都有',link: '/one/',},
+  { text: '入门',link: '/rookie/',},
+  { text: '修养',link: '/behavior/',},
+  { text: '专业基础',link: '/basic/',},
+  { text: '软件设计',link: '/design/',},
+  { text: '高手成长',link: '/grow/',},
+  { text: '学习方式方法',link: '/learn/',},
   { text: '留言板', link: '/message-board/' },
 ]
